4e666d58df092f2be879cf67d76446dfb658bfea
root <root@python.local>
2022-12-22 12:20:55 +0300

    Lesson4 Task1

--- Lesson4/bubble_method.py
+++ Lesson4/bubble_method.py
@@ -6,7 +6,7 @@ print(f"случайная последовательность: {a}")
 
 for i in range(len(a) - 1):
     count = 0
-    for j in range(len(a) - i - 1):
+    for j in range(len(a)-i - 1):
         if a[j] > a[j + 1]:
             a[j],a[j + 1] = a[j + 1],a[j]
             count += 1
