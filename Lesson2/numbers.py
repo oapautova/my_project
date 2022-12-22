5ce68c9764c23764671a3e321e004d11f669d71b
root <root@python.local>
2022-12-22 12:29:41 +0300

    Lesson2 Task1

--- Lesson2/numbers.py
+++ Lesson2/numbers.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-num1, num2 = float(input('Введите первое число: ')), float(input('Введите второе число: '))
-print("Сумма:", num1, "+", num2, "=",  num1 + num2)
-print("Разность:",num1, "-", num2, "=", num1 - num2)
-print("Произведение:", num1, "*", num2, "=", num1 * num2)
-print("Частное:", num1, "/", num2, "=", num1 / num2)
-print("Возведение в степень:", num1, "**", num2, "=", num1 ** num2)
-print("Деление по модулю:", num1, "%", num2, "=", num1 % num2)
-print("Целочисленное деление:", num1, "//", num2, "=", num1 // num2)
+
+num1 = float(input('Введите первое число: ')) 
+num2 = float(input('Введите второе число: '))
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} ** {num2} = {num1 ** num2}")
+print(f"{num1} % {num2} =  {num1 % num2}")
+print(f"{num1} // {num2} = {num1 // num2}")
