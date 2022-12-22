5d87ca4cc3b9c6a018ef86ecf03d206930eebcb5
root <root@python.local>
2022-12-22 11:45:12 +0300

    Lesson5 Task2

--- Lesson5/passwords.py
+++ Lesson5/passwords.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
-print("Вводимый пароль должен содержать не менее 6 символов, должен содержать хотя бы одну цифру, не должен состоять только из цифр, не должен содержать слово 'password'.")
-def func(c):
+
+print("Вводимый пароль должен содержать не менее 6 символов.")
+print("Должен содержать хотя бы одну цифру.") 
+print("Не должен состоять только из цифру") 
+print("не должен содержать слово 'password'.")
+
+def num_dig(c):
+    """Считает количество цифр
+
+    Пинимает:
+        строку
+    Возвращает:
+        количество цифр в строке
+    
+    """
+
     d = 0
     for x in c:
         if x.isdigit():
@@ -9,12 +23,14 @@ def func(c):
 
 while True:
     pas = input("Введите пароль:")
-    a = len(pas)
-    quantity_digit = func(pas)
+    lenght = len(pas)
+    quantity_digit = num_dig(pas)
     pas1 = pas.lower()
     s = pas1.find("password")
-    if a < 6 or quantity_digit == a  or quantity_digit == 0 or s != -1:
-        q = input("Пароль не корректный. Для выхода из программы введите 'выход', для повторного ввода пароля, нажмите enter или введите любой символ: ").lower().strip()
+    if lenght < 6 or quantity_digit == lenght  or quantity_digit == 0 or s != -1:
+        print("Пароль не корректный. Для выхода из программы введите 'выход'.")
+        print("для повторного ввода пароля, нажмите enter или введите любой символ:")
+        q = input().lower().strip()
         if q == "выход":
             break
         else:
