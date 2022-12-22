dc0f5e419a0686fde29bb60fec56ec86a9fde12f
root <root@python.local>
2022-12-22 12:28:59 +0300

    Lesson2 Task4

--- Lesson2/area_of_a_circle.py
+++ Lesson2/area_of_a_circle.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import math
 r = float(input("Введите значение радиуса окружности: "))
-print("Площадь окружности: ", math.pi * (r ** 2))  
+print("Площадь окружности: ", math.pi * r**2)  
